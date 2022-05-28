b45fd93665c91df05a9a3107cbf8c81d95c1eff2
Gilmar Silva <gilmarheavensilva@hotmail.com>
2022-05-28 12:42:30 -0300

    fix request body

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ const app = express()
 const port = 3000
 
 app.post("/notifications", (request, response) => {
-    console.log("requet", request)
+    console.log("requet", request.body)
 })
 
 app.listen(port, () => {
