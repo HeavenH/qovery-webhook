b6e69c22a586b1f27fc120332397794214f1bbb3
Gilmar Silva <gilmarheavensilva@hotmail.com>
2022-05-29 12:39:55 -0300

    add embeded

--- main.ts
+++ main.ts
@@ -19,6 +19,8 @@ app.post("/notifications", async (request, response) => {
 
     const channel = client.channels.cache.get('980083024672718939');
 
+    console.log("BODY", request.body.release)
+
     if (request.body.action == "created") {
         // @ts-ignore
         let html_url = request.body.release.html_url
@@ -37,6 +39,9 @@ app.post("/notifications", async (request, response) => {
             .setURL(payload.html_url)
             .setDescription(payload.body)
             .setTitle(payload.release_name)
+            .setThumbnail("https://e3ba6e8732e83984.cdn.gocache.net/uploads/image/file/72484/regular_conta-azul.png")
+            .setImage("https://e3ba6e8732e83984.cdn.gocache.net/uploads/image/file/72484/regular_conta-azul.png")
+            .addFields()
         // @ts-ignore
         channel.send({ embeds: [formatedPayload]});
     }
